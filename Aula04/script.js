aea434f21f94fd3eef2a6ab10babb0ffe8213bf3
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2021-11-03 15:36:46 -0300

    based

---
+++ Aula04/script.js
@@ -0,0 +1,7 @@
+var joao = 'Minha Primeira Mensagem'
+
+    window.alert(joao)
+    window.confirm('Está gostando de JS?')
+    var joaozin = window.prompt("what is your name?") // pergunta o nome em inglês
+
+    alert(joaozin)
