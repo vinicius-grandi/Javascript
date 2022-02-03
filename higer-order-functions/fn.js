bbf5f63bcdd6a2685dd6c3368b4fc0dee3130e65
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-03 09:47:43 -0300

    funções de 1,9m

---
+++ higer-order-functions/fn.js
@@ -0,0 +1,9 @@
+const getName = (name) => (greeting) => `${greeting}, ${name}`;
+
+const firstG = getName('João');
+const secondG = getName('Lula');
+const thirdG = gertName('Túlio');
+
+console.log(firstG('Salve'));
+console.log(secondG('Tranquilidade'));
+console.log(thirdG('Muito bem vindo'));
